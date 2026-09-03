154f50e64cb837a48854ca5215a9a9c8057289d6
Isidro A. de Ojalvo Abelló García <izzyabellog@gmail.com>
2026-09-02 22:00:04 -0600

    Aprendiendo a testear en C

--- server/hello/src/main.c
+++ server/hello/src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "sum.c"
 
 int main(void) {
     printf("Hello, world!!!\n");
+
+    int n = sum(5, 3);
+
+    printf(n);
+
     return 0;
 }
---
+++ server/hello/src/test_sum.c
@@ -0,0 +1,8 @@
+# include "sum.h"
+
+int main () 
+{
+    if (sum(5, 4) != 9)
+        return 1;
+    return 0;
+}
