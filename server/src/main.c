aa9edf20d4f4bd9d0737dc400e8b413a7829918d
Isidro A. de Ojalvo Abelló García <izzyabellog@gmail.com>
2026-09-09 20:36:28 -0600

    banderas agregadas

--- server/src/main.c
+++ server/src/main.c
@@ -10,18 +10,26 @@
 #include <sys/types.h>
 #include <string.h>
 #include <stdarg.h>
+#include <errno.h>
 
 #define DEFAULT_PORT 1234
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 1024
+
+int help_fl = 0;
+int  log_fl = 0;
+char const* port_fl = NULL;
 
 void notify(const char *format, ...) 
 {
     va_list args;
     va_start(args, format);
 
-    printf("[SERVER]: ");
-    vprintf(format, args);
-    printf("\n");
+    if (log_fl == 1)
+    {
+        printf("[SERVER]: ");
+        vprintf(format, args);
+        printf("\n");
+    }
 
     va_end(args);
 }
@@ -31,15 +39,68 @@ void notify_error(char* message)
     perror(strcat("[SERVER - ERROR] ", message));
 }
 
-int set_port(int argc, char const* argv[]) 
+void show_help ()
 {
-    // Implement here a safe method...
-    return DEFAULT_PORT;
+    printf("Mostrar mensaje de ayuda...");
+}
+
+void get_input(int argc, char const* argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        char const* param = argv[i]; 
+        if (strcmp(param, "-h") == 0 || strcmp(param, "--help") == 0)
+        {
+            show_help();
+            exit(0);
+        }
+
+        if (strcmp(param, "-l") == 0 || strcmp(param, "--log") == 0)
+            log_fl = 1;
+
+        if ((strcmp(param, "-p") == 0 || strcmp(param, "--port") == 0 || strcmp(param, "--puerto") == 0)
+            && argc != i)
+            port_fl = argv[++i];
+    }
+}
+
+int set_port() 
+{
+    if (port_fl == NULL) 
+        return DEFAULT_PORT;
+
+    char *endptr;
+    errno = 0;  
+
+    long input = strtol(port_fl, &endptr, 10);
+
+    if (endptr == port_fl) 
+    {
+        notify_error("No se encontró ningún número de puerto válido.");
+        notify("Puerto inválido, levantando en puerto %d.", DEFAULT_PORT);
+        return DEFAULT_PORT;
+    } 
+    if (*endptr != '\0')
+    {
+        notify("Advertencia: Se encontraron caracteres no numéricos extra: %s\n", endptr);
+    }
+    if (errno != 0) 
+    {
+        perror("Error de rango/desbordamiento.");
+        notify("Puerto inválido, levantando en puerto %d.", DEFAULT_PORT);
+        return DEFAULT_PORT;
+    }
+
+    return (int)input;
 }
 
 int main(int argc, char const* argv[])
 {
-    int port = set_port(argc, argv);
+    get_input(argc, argv);
+
+    int port = set_port();
+
+    notify("Levantando servidor en el puerto %d...", port);
 
     int servSockD = socket(AF_INET, SOCK_STREAM, 0);
     if (servSockD < 0)
@@ -47,7 +108,7 @@ int main(int argc, char const* argv[])
         notify_error("Error al crear el socket");
         return 1;
     }
-    notify("Socket creado correctamente.\n");
+    notify("Socket creado correctamente.");
 
 
     char serMsg[BUFFER_SIZE] = "Message from the server to the client \'Hello Client\' ";
@@ -58,29 +119,29 @@ int main(int argc, char const* argv[])
     servAddr.sin_port = htons(port);
     servAddr.sin_addr.s_addr = INADDR_ANY;
 
-    notify("Vinculando socket al puerto %d...\n", port);
+    notify("Vinculando socket al puerto %d...", port);
     if (bind(servSockD, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
     {
         notify_error("Error en bind");
         return 1;
     }
-    notify("Socket vinculado al puerto %d.\n", port);
+    notify("Socket vinculado al puerto %d.", port);
 
-    notify("Esperando conexiones...\n");
+    notify("Esperando conexiones...");
     if (listen(servSockD, SOMAXCONN) < 0)
     {
         notify_error("Error en listen");
         return 1;
     }
-    notify("Servidor escuchando en el puerto %d.\n", port);
+    notify("Servidor escuchando en el puerto %d.", port);
 
-    notify("Esperando a que se conecte un cliente...\n");
+    notify("Esperando a que se conecte un cliente...");
     int clientSocket = accept(servSockD, NULL, NULL);
     if (clientSocket < 0) {
         notify_error("Error en accept");
         return 1;
     }
-    notify("¡Cliente conectado!\n");
+    notify("¡Cliente conectado!");
 
     send(clientSocket, serMsg, sizeof(serMsg), 0);
 
